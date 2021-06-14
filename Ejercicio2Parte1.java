2cca61b29bb5f740a8c4eae6a49518abeecd63af
MaríaPérez <yohanalpz17@gmail.com>
2021-06-14 13:19:56 -0600

    Ejercicio2Parte1

---
+++ Ejercicio2Parte1.java
@@ -0,0 +1,20 @@
+
+    import java.util.Scanner;
+
+    public class Ejercicio2Parte1 {
+        static Scanner entrada = new Scanner(System.in);
+
+        public static void main(String[] args) {
+    
+           int pais, cap;
+           System.out.print("Ingresar un pais: ");
+           pais = entrada.nextInt();
+        
+
+          System.out.println("Ingresar capital: ");
+          cap = entrada.nextInt();
+         
+           System.out.println("La capital del pais es: ");
+        }
+      
+    }
