d61426f6b7f843f1805ac6927ac4f0e4298992f3
MaríaPérez <yohanalpz17@gmail.com>
2021-06-14 13:22:11 -0600

    Ejercicio5Parte1

---
+++ Ejercicio5Parte1.java
@@ -0,0 +1,15 @@
+
+    import java.util.Scanner;
+
+public class Ejercicio5Parte1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        System.out.println("Ingrasar un numero:");
+        numero = sc.nextInt();
+        System.out.println("Numero ingresado: " + numero );
+        System.out.println("Siguiente numero " + " = " + (numero + 1));
+}
+}
